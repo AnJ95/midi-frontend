325493845c5700ede0e0c672c7fcd6c76fc9eae4
Angelo Aracri <angeloaracri@yahoo.de>
2024-02-11 15:27:11 +0100

    Enable sending any test messages

--- src/scripts/component/SocketDebug.tsx
+++ src/scripts/component/SocketDebug.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {ReadyState} from 'react-use-websocket';
 import useMidiSocket from './../hooks/useMidiSocket'
 
@@ -19,13 +19,23 @@ export default function SocketDebug() {
     const [messageHistory, setMessageHistory] = useState<Json[]>([]);
     const [sendMessage, lastMessage, isOpen, readyState] = useMidiSocket("*");
 
+    const onClickSendMessage = () => {
+        try {
+            if (inputElement.current) {
+                const json = JSON.parse(inputElement.current.value)
+                sendMessage(json);
+            }
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    const inputElement = useRef<HTMLInputElement>(null);
+
     useEffect(() => {
         if (lastMessage !== null) {
             setMessageHistory((prev) => prev.concat(lastMessage));
         }
     }, [lastMessage, setMessageHistory]);
-
-    const handleClickSendMessage = useCallback(() => sendMessage({type: "debug", text: "hallo"}), []);
 
     const messageHistoryStr = messageHistory
         .map((message) => {
@@ -44,7 +54,9 @@ export default function SocketDebug() {
             <FlexRow stretch style={{height: "100%"}}>
                 <div>
                     <p>WebSocket ReadyState: {READY_STATES[readyState]}</p>
-                    <button onClick={handleClickSendMessage} disabled={!isOpen}>TestMsg</button>
+                    <input ref={inputElement} defaultValue={'{"type": "debug", "text": "hallo"}'}/>
+                    &nbsp;
+                    <button onClick={onClickSendMessage} disabled={!isOpen}>Send</button>
                 </div>
                 <textarea readOnly style={{height: "100px", whiteSpace: "pre"}} value={messageHistoryStr}/>
             </FlexRow>
